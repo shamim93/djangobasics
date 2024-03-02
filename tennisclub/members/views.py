f55e3908b6978bb0ba08c8292433a6dd3134ee02
Shamim Hossen <shamim41.cse@gmail.com>
2024-03-02 23:00:22 +0100

    views updated

--- tennisclub/members/views.py
+++ tennisclub/members/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
 
 # Create your views here.
 
 
 def members(request):
-    return HttpResponse("Hello,world")
+    template = loader.get_template("index.html")
+    return HttpResponse(template.render())
